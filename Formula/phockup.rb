f4b6ab98bedbb65e227d8516c92faa540c094bda
Ivan Dokov <ivangdokov@gmail.com>
2017-07-05 00:33:13 +0300

    Changed checksum

--- Formula/phockup.rb
+++ Formula/phockup.rb
@@ -2,7 +2,7 @@ class Phockup < Formula
   desc "Organize photos and videos in folders by year, month and day."
   homepage "https://github.com/ivandokov/phockup"
   url "https://github.com/ivandokov/phockup/archive/1.3.2.tar.gz"
-  sha256 "b0acec23b0464066b44646011ecd180f3a4172e7419d0fc6a4ec74d001ce5a59"
+  sha256 "d528699255abd246553bf2ed79f34dda1b650f0dd604bb80422c1f1b1e59829c"
 
   depends_on "exiftool"
   depends_on "python3"
