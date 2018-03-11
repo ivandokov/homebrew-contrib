73a76361cc77e2591d7b395f89ea5a5f6417483e
Ivan Dokov <ivangdokov@gmail.com>
2018-03-11 23:14:48 +0200

    1.5.2

--- Formula/phockup.rb
+++ Formula/phockup.rb
@@ -1,8 +1,8 @@
 class Phockup < Formula
   desc "Organize photos and videos in folders by year, month and day."
   homepage "https://github.com/ivandokov/phockup"
-  url "https://github.com/ivandokov/phockup/archive/1.5.1.tar.gz"
-  sha256 "09e10e6acfde144c57ed03c581fe2e2364b38288a2b64b287b85b648db466865"
+  url "https://github.com/ivandokov/phockup/archive/1.5.2.tar.gz"
+  sha256 "47fded1c468aa3789a9b3bfff7f276019d6ee4ff311a9f2b870143445fd28ebf"
 
   depends_on "exiftool"
   depends_on "python3"
