27b95c6a5264a57e92394acb522352e32175c2ba
Ivan Dokov <ivan@jetspark.io>
2021-01-07 10:32:31 +0200

    Fixed sha

--- Formula/phockup.rb
+++ Formula/phockup.rb
@@ -2,7 +2,7 @@ class Phockup < Formula
   desc "Organize photos and videos in folders by year, month and day."
   homepage "https://github.com/ivandokov/phockup"
   url "https://github.com/ivandokov/phockup/archive/1.5.13.tar.gz"
-  sha256 4561a5334d6a28def983f5b30823590991e788f298cb43e378e7845c62bbeb9a
+  sha256 32bcbb40653384f2f9e5fb49b2b8dbbcf59f63798b328571b69ac5aca1b21f39
 
   depends_on "exiftool"
   depends_on "python3"
